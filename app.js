58b7bf98d91c1a36cee569488620d6a264887657
Thinh Pham <thinhpham1842005@gmail.com>
2026-01-29 07:41:36 -0500

    Code is work, everything run well

--- app.js
+++ app.js
@@ -1,4 +1,30 @@
+// Interactive Dark and Light Mode
+function toggleMode() {
+  document.body.classList.toggle("light");
+  document.body.classList.toggle("dark");
+}
+
 //  https://hp-api.onrender.com/api/characters
+let searchInput = document.querySelector(".input__search");
+let characterList = document.querySelector(".character__container");
+let allCharacters = [];
+
+searchInput.addEventListener("keyup", (event) => {
+  const searchTarget = event.target.value.toLowerCase();
+  const filterCharacter = allCharacters.filter((character) => {
+    return (
+      character.name.toLowerCase().includes(searchTarget) ||
+      character.id.toLowerCase().includes(searchTarget)
+    );
+  });
+  renderUsersCharacter(filterCharacter);
+  // if (searchTarget === 0 && !searchTarget) {
+  //   fetchCharacterUser();
+  // }
+  if (searchTarget.length === 0) {
+    renderUsersCharacter(allCharacters.slice(0, 6));
+  }
+});
 
 async function fetchCharacterUser() {
   try {
@@ -6,28 +32,40 @@ async function fetchCharacterUser() {
     if (!response.ok) {
       console.log("Cannot realize this api");
     }
-    const data = await response.json();
-    renderUsersCharacter(data);
+    allCharacters = await response.json();
+    renderUsersCharacter(allCharacters.slice(0, 6));
   } catch (error) {
     console.log(error);
   }
 }
 
 function renderUsersCharacter(characters) {
-  let characterList = document.querySelector(".character__container");
   const characterRender = characters
-    .slice(0, 10)
+
     .map((character) => {
-      return ` <div class="movie">
-          <img src="${character.image}" class="movie__image" alth="Movie Image Title" />
+      return ` 
+      <div class="movie">
+     
+         <img src="${character.image || "placeholder.jpg"}" 
+     onerror="this.src='https://via.placeholder.com/198x288?text=No+Image'" 
+     class="movie__image" alt="${character.name}" />
           <div class="character__info">
             <h3 class="character__name">${character.name}</h3>
-            <span class="date__of__birth">${character.dateOfBirth}</span>
+            <span class="date__of__birth">${character.gender}</span>
             <button class="more__details__info">More Details</button>
-          </div>`;
+            
+          </div>
+        </div>`;
     })
     .join("");
   characterList.innerHTML = characterRender;
 }
+function loadingResult(event) {
+  event.preventDefault();
+}
 
 fetchCharacterUser();
+
+const formSubmit = document.querySelector("form");
+
+formSubmit.addEventListener("submit", loadingResult);
